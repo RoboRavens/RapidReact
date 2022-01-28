71d0a7cefc3091311de40b21e6a142add1c6d1ed
Tair K <54789486+f873@users.noreply.github.com>
2022-01-27 22:09:22 -0500

    Create IntakeExtendAndReverseCommand
    NOT FINISHED YET Just Made a home base for the command

---
+++ IntakeExtendAndReverseCommand.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class IntakeExtendAndReverseCommand extends CommandBase {
+  
+  private final IntakeSubsystem intakeSubsystem;
+
+ 
+  public IntakeExtendAndReverseCommand(IntakeSubsystem intakeSubsystem) {
+    IntakeSubsystem = intakeSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intakeSubsystem);
+  }
+
+ 
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
