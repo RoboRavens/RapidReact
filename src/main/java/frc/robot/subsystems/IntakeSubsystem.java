f3f10ed903407ce78e2d6a06e1954bfb65fd2bec
Tair K <karanfilitair@outlook.com>
2022-01-28 19:09:25 -0500

    Changed "TALONSRX" To TALONFX

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -8,7 +8,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -16,13 +16,13 @@ import frc.robot.RobotMap;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-  private TalonSRX _intakeMotor;
+  private TalonFX _intakeMotor;
   private Solenoid _intakeExtend;
   private Solenoid _intakeRetract;
 
   public IntakeSubsystem() {
     this.initialize();
-    _intakeMotor = new TalonSRX(RobotMap.INTAKE_MOTOR);
+    _intakeMotor = new TalonFX(RobotMap.INTAKE_MOTOR);
     _intakeExtend = new Solenoid(RobotMap.INTAKE_EXTEND_SOLENOID, 0);
     _intakeRetract = new Solenoid( RobotMap.INTAKE_RETRACT_SOLENOID, 0);
   }
