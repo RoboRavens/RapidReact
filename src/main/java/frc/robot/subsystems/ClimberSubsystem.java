31d3735f708c336319bc9846d6454803b12106b4
milligansarah <sarahkm246@gmail.com>
2022-02-04 18:17:22 -0500

    Created The Climber Subsystem

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase {
+    public ClimberSubsystem() {
+
+    }
+    
+    @Override
+    public void periodic() {
+        // TODO Auto-generated method stub
+        super.periodic();
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // TODO Auto-generated method stub
+        super.simulationPeriodic();
+    }
+}
