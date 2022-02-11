01b2e77538bb5889a6db9a16d5f4acce13d1097b
Tair K <karanfilitair@outlook.com>
2022-02-11 00:23:38 -0500

    Added Command groups back after Github merge mistake
    i have reviewed the mistake and added everything back it wasn't much

--- src/main/java/frc/robot/commands/IntakeCollectCommandGroup.java
+++ src/main/java/frc/robot/commands/IntakeCollectCommandGroup.java
@@ -5,14 +5,12 @@ import frc.robot.subsystems.ConveyanceSubsystem;
 import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.IntakeExtenderSubsystem;
 
-public class ConveyanceIntakeCommandGroup extends ParallelCommandGroup{
-    public void ConveyanceEjectCommandGroup(ConveyanceSubsystem conveyanceSubsystem, FeederSubsystem feederSubsystem, IntakeExtenderSubsystem intakeExtenderSubsystem) {
+public class IntakeCollectCommandGroup extends ParallelCommandGroup{
+    public IntakeCollectCommandGroup(ConveyanceSubsystem conveyanceSubsystem, IntakeExtenderSubsystem intakeExtenderSubsystem) {
         addCommands(
             new IntakeExtendCommand(),
             new ConveyanceCollectCommand(),
             new IntakeRetractCommand()
         );
-
-     }
-    
+    }
 }
