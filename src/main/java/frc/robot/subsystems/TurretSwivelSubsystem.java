44ab166a1cd43896f476eea97d0537e0bb5b5cd7
red4stone <creeperinthegarden@gmail.com>
2022-02-24 20:59:33 -0500

    Update TurretSwivelSubsystem.java

--- src/main/java/frc/robot/subsystems/TurretSwivelSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSwivelSubsystem.java
@@ -33,6 +33,7 @@ public class TurretSwivelSubsystem extends SubsystemBase {
         SmartDashboard.putNumber("Turret Angle", getAngle());
         SmartDashboard.putNumber("Turret RAW Encoder", _turretMotor.getSelectedSensorPosition());
         SmartDashboard.putNumber("Turret Target", _shot.target);
+        SmartDashboard.putNumber("Turret Volt", _turretMotor.getMotorOutputVoltage());
     }
 
     @Override
