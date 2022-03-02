2f433fd2de8dc4b8279b64f10537398a4008136e
milligansarah <98560997+milligansarah@users.noreply.github.com>
2022-03-01 21:00:09 -0500

    Saved Files

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.controls.ButtonCode;
 import frc.controls.Gamepad;
 import frc.robot.commands.ClimberDefaultBrakeCommand;
-import frc.robot.commands.CompressorTurnOffWhileShootingAndClimbingCommand;
+import frc.robot.commands.CompressorTurnOffWhileShootingOrClimbingCommand;
 import frc.robot.commands.CompressorTurnOffWhileShootingOrClimbingCommand;
 import frc.robot.commands.ConveyanceCollectCommand;
 import frc.robot.commands.ConveyanceEjectCommand;
--- src/main/java/frc/robot/commands/CompressorTurnOffWhileShootingOrClimbingCommand.java
+++ src/main/java/frc/robot/commands/CompressorTurnOffWhileShootingOrClimbingCommand.java
@@ -26,6 +26,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    //**refer to slack for further info
     // figure out a way to set the IsClimbing and IsShooting to true when the robot is actually climbing or shooting.
     // probably will have to do this within the Climbing and Shooting Subsystems?
     if(Robot.COMPRESSOR_SUBSYSTEM.IsClimbing() || Robot.COMPRESSOR_SUBSYSTEM.IsShooting()) {
