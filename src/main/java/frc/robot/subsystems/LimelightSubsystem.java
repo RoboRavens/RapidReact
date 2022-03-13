357fa4ecc84bdbe36e58f5164ca1d3ccfe1b8890
Tair K <karanfilitair@outlook.com>
2022-03-13 13:08:53 -0400

    Made Limelight code look easier
    I Removed any unnecessary limelight code. I was reading the limelight documentation and found that there was some code that was unneeded.

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -13,7 +13,6 @@ public class LimelightSubsystem extends SubsystemBase {
     NetworkTableEntry tl = table.getEntry("Tl");
     NetworkTableEntry tv = table.getEntry("Tv");
     NetworkTableEntry ts = table.getEntry("ts");
-    NetworkTableEntry ledMode = table.getEntry("ledMode");
     //read values periodically
     double x = tx.getDouble(0.0);
     double y = ty.getDouble(0.0);
@@ -21,33 +20,15 @@ public class LimelightSubsystem extends SubsystemBase {
     double l = tl.getDouble(0.0); // The pipeline’s latency contribution (ms) Add at least 11ms for image capture
     double v = tv.getDouble(0.0); // Whether the limelight has any valid targets (0 or 1)
     double s = ts.getDouble(0.0); // Skew or rotation (-90 degrees to 0 degrees)
-    public int _ledState = 3;
+    public int ledMode = 3;
     public int camMode = 0; 
 
-    public void toggleLED() {
-		if (_ledState == 0) {
-			setBothLEDOn();
-		} else if (_ledState == 3) {
-			setOneLEDOn();
-		}
+  public void turnLEDOff() {
+		ledMode = 1;
 	}
 
-
-    public void setBothLEDOn() {
-		_ledState = 3;
-	}
-
-	public void turnLEDOff() {
-		ledMode.setNumber(1);
-	}
-
-	public void setOneLEDOn() {
-		_ledState = 0;
-	}
-
-   
 	public void turnLEDOn() {
-		ledMode.setNumber(3);
+		ledMode = 3;
 	}
     
 }
