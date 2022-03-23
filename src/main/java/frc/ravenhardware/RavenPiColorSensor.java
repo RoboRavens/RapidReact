c245d903bd4e50d471d582588d6dd863a4607caa
red4stone <creeperinthegarden@gmail.com>
2022-03-22 20:50:55 -0400

    Streamlined logic and readability

--- src/main/java/frc/ravenhardware/RavenPiColorSensor.java
+++ src/main/java/frc/ravenhardware/RavenPiColorSensor.java
@@ -9,34 +9,29 @@ public class RavenPiColorSensor extends PicoColorSensor{
     }
 
     /**
-     * Gets ball in front of sensor 0
+     * Gets ball in front of sensor of choice
+     * @param pos The enum position of the sensor of choice: either ENTRY or EXIT
      * @return Returns an enum of the detected color, either UNKNOWN, RED, or BLUE
      */
-    public RavenPiColor getBallType0() {
-        if(Math.abs(getRawColor0().red - getRawColor0().blue) > Constants.BALL_COLOR_THRESHOLD_0) {
-            if(getRawColor0().red > getRawColor0().blue) {
-                return(RavenPiColor.RED);
-            } else {
-                return(RavenPiColor.BLUE);
-            }
-        } else {
-            return(RavenPiColor.UNKNOWN);
-        }
-    }
+    public RavenPiColor getBallType(RavenPiPosition pos) {
+        RawColor colorval;
 
-    /**
-     * Gets ball in front of sensor 1
-     * @return 0 if ball is undecided color / 1 if ball is red / 2 if ball is blue
-     */
-    public int getBallType1() {
-        if(Math.abs(getRawColor1().red - getRawColor1().blue) > Constants.BALL_COLOR_THRESHOLD_1) {
-            if(getRawColor1().red > getRawColor1().blue) {
-                return(1);
-            } else {
-                return(2);
-            }
+        switch(pos) {
+            case ENTRY:
+                colorval = getRawColor0();
+                break;
+            default: // Can't be anything other than EXIT if pos is not ENTRY
+                colorval = getRawColor1();
+        }
+
+        if(Math.abs(colorval.red - colorval.blue) < Constants.BALL_COLOR_THRESHOLD_0) {
+            return(RavenPiColor.UNKNOWN); // Stop if difference in color is not trustworthy enough
+        }
+
+        if(colorval.red > colorval.blue) {
+            return(RavenPiColor.RED);
         } else {
-            return(0);
+            return(RavenPiColor.BLUE);
         }
     }
 }
