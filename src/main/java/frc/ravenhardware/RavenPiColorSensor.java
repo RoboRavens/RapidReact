3989f4a03184ec1cd4a59a13fd3d420faeb15da3
red4stone <creeperinthegarden@gmail.com>
2022-03-22 20:59:03 -0400

    Ternary operator use for simplified code

--- src/main/java/frc/ravenhardware/RavenPiColorSensor.java
+++ src/main/java/frc/ravenhardware/RavenPiColorSensor.java
@@ -14,15 +14,7 @@ public class RavenPiColorSensor extends PicoColorSensor{
      * @return Returns an enum of the detected color, either UNKNOWN, RED, or BLUE
      */
     public RavenPiColor getBallType(RavenPiPosition pos) {
-        RawColor colorval;
-
-        switch(pos) {
-            case ENTRY:
-                colorval = getRawColor0();
-                break;
-            default: // Can't be anything other than EXIT if pos is not ENTRY
-                colorval = getRawColor1();
-        }
+        RawColor colorval = pos == RavenPiPosition.ENTRY ? getRawColor0() : getRawColor1();
 
         if(Math.abs(colorval.red - colorval.blue) < Constants.BALL_COLOR_THRESHOLD_0) {
             return(RavenPiColor.UNKNOWN); // Stop if difference in color is not trustworthy enough
