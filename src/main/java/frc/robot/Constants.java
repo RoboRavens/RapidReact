ab2271840b71e4c04e54c3de905e065cb211d190
red4stone <creeperinthegarden@gmail.com>
2022-03-29 20:45:21 -0400

    Gear ratio changes
    Added the gear ratio for the small gear to the giant aluminum gear (caused encoder_to_turret_ratio to multiply by 12, needs testing!).

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -185,8 +185,9 @@ Y-offset constants in order:
     public static final int TURRET_IDX = 0;
     public static final int TURRET_TIMEOUT_MS = 100;
     public static final int TURRET_RANGE = 150; //Degrees of motion in either way (180 means full movement both ways)
-    public static final double TURRET_GEAR_RATIO = 1.0 / 10.0; // Motor is one-tenth speed due to gearbox
-    public static final double ENCODER_TO_TURRET_RATIO = TALONFX_TICKS_PER_REVOLUTION / 360.0 / TURRET_GEAR_RATIO; // Multiply encoder by this to find angle of turret
+    public static final double TURRET_GEARBOX_RATIO = 1.0 / 10.0; // Motor is one-tenth speed due to gearbox
+    public static final double TURRET_LARGE_GEAR_RATIO = 10.0 / 128.0;
+    public static final double ENCODER_TO_TURRET_RATIO = (TALONFX_TICKS_PER_REVOLUTION / 360.0) / TURRET_GEARBOX_RATIO / TURRET_LARGE_GEAR_RATIO; // Multiply encoder by this to find angle of turret
     public static final double TURRET_AIM_ALLOWANCE = 2; //Degrees of allowance to say that the turret has "reached" its target
     public static final double TURRET_MISS_OFFSET = 35;
 
