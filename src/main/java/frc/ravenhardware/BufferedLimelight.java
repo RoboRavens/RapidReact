a6674a6da2c004601e1d3d755d150b8109f9396f
Tair K <karanfilitair@outlook.com>
2022-05-06 08:41:39 -0400

    comments cleanup and buffered class made

---
+++ src/main/java/frc/ravenhardware/BufferedLimelight.java
@@ -0,0 +1,5 @@
+package frc.ravenhardware;
+
+public class BufferedLimelight {
+    
+}
--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -10,18 +10,12 @@ public class LimelightSubsystem extends SubsystemBase {
   private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
   private NetworkTableEntry _tx = table.getEntry("tx");
   private NetworkTableEntry _ty = table.getEntry("ty");
-  private NetworkTableEntry _ta = table.getEntry("ta"); // area
-  // private NetworkTableEntry _tl = table.getEntry("Tl"); // The pipeline’s latency contribution (ms) Add at least 11ms for image capture
-  private NetworkTableEntry _tv = table.getEntry("tv"); // Whether the limelight has any valid targets (0 or 1)
-  // private NetworkTableEntry _ts = table.getEntry("ts"); // Skew or rotation (-90 degrees to 0 degrees)
+  private NetworkTableEntry _ta = table.getEntry("ta"); 
+  private NetworkTableEntry _tv = table.getEntry("tv"); 
+ 
 
   NetworkTableEntry ledMode = table.getEntry("ledMode");
-  // double x = tx.getDouble(0.0);
-  // double y = ty.getDouble(0.0);
-  // double area = ta.getDouble(0.5);
-  // double l = tl.getDouble(0.0); // The pipeline’s latency contribution (ms) Add at least 11ms for image capture
-  // double v = tv.getDouble(0.0); // Whether the limelight has any valid targets (0 or 1)
-  // double s = ts.getDouble(0.0); // Skew or rotation (-90 degrees to 0 degrees)
+
   public int _ledState = 3;
   public int camMode = 0;
   @Override
