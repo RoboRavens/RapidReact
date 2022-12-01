601ce8c24a39e1be87df78a3000068146a8521d3
Tair K <karanfilitair@outlook.com>
2022-12-01 09:36:08 -0500

    Math to Calculate Pressure by assuming supply voltage

--- src/main/java/frc/ravenhardware/PressureSensorTwo.java
+++ src/main/java/frc/ravenhardware/PressureSensorTwo.java
@@ -4,16 +4,23 @@ import edu.wpi.first.wpilibj.AnalogInput;
 
 public class PressureSensorTwo {
     
-    public PressureSensorTwo(int i) {
-    
+   //this only would work with doubles so I just implemented them as a double
+   public double supplyVoltage = 5;
+   public double x = 250;
+   
+   public PressureSensorTwo(int i) {
+     
+   
     
     }
 
      AnalogInput analog = new AnalogInput(3);
 
         public double getPressure() {
-
-           return analog.getVoltage();
-        }
-
-    }
+         
+         return analog.getVoltage() / supplyVoltage * x - 25;
+         //CALCULATING PRESSURE
+         
+       }
+       
+}
